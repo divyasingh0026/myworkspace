d0e2251ccc2f89a3ba13d949e279144bd63e9628
divyasingh0026 <daisyworlddivya@gmail.com>
2025-09-14 19:16:15 +0530

    commit

---
+++ practice am sir unit2.c
@@ -0,0 +1,23 @@
+#inlcude<stdio.h>
+#define M
+int main(){
+int a,b,c ;
+printf("program\n");
+
+ a=10;
+ b=20;
+ c=30;	
+	(a>b)?a:b ;
+	(c>a)?c:a ;
+	
+	printf("the biggest number is %d",a);
+	
+	
+	printf("the biggest number is %d",b);
+	
+	
+	printf("the biggest number is %d",c);
+	
+	return 0;
+
+}
